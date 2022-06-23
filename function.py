b35a0702c2eb4b2c7d3a1de6ecb6046dcc377faf
VinayakVATalekar <105624044+VinayakVATalekar@users.noreply.github.com>
2022-06-23 17:49:52 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,17 @@
+def prime(x):
+     
+    for i in range(2,x):
+        
+        if x%i==0:
+            return "not prime"
+    return 'prime'    
+
+
+
+x=int(input("enter a nno"))
+if x>2:
+    print(prime(x))
+elif x==2:
+    print('prime')
+else:
+    print('not prime')

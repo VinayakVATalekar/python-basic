0b3666caafb0cee241057a5b45f825fc84792fb0
VinayakVATalekar <105624044+VinayakVATalekar@users.noreply.github.com>
2022-06-23 17:25:34 +0530

    Create strmanip.py

---
+++ strmanip.py
@@ -0,0 +1,41 @@
+name = "vinayak"
+#print in capital
+print(name.capitalize())
+
+name = "SAURABH"
+#print in lower
+print(name.lower())
+
+#return index
+print(name.count("a"))
+
+#check to all is alphabet
+print(name.isalpha())
+#check for lower case
+print(islower(name))
+#upper case
+print(isuperer(name))
+name = "he was good"
+#replace all element
+print(name.replace("o", "h"))
+#replace upto spacific count(2)
+quote = "music is life"
+print(quote.replace("i", "non", 2))
+
+#returns true if startswith
+text = "hello brother  "
+result = text.startswith("hello")
+
+
+#to check ending
+name = "honeysing"
+print(name.endswith("o"))
+
+language = "French"
+print(language.endswith("ch"))
+
+#split string from " "
+name = "life is good"
+x=name.split(" ")
+
+
